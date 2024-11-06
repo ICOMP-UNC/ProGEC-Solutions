9ea34d58c3482aa4d14f99af5460e1b0669445d2
EmiCasttro01 <emiliano.castro@mi.unc.edu.ar>
2024-11-06 14:02:39 -0300

    Funcion del monitoreo activo (analyze)

--- include/util.h
+++ include/util.h
@@ -1,5 +1,16 @@
 #define _MAX_VIB_N 50 // cantidad maxima de vibraciones a guardar
 
+#define THRESHOLD_VIB_FREQ_H 1000
+#define THRESHOLD_VIB_FREQ_M 500
+#define THRESHOLD_VIB_FREQ_L 0
+
+#define THRESHOLD_HUM_H 1000
+#define THRESHOLD_HUM_M 500
+#define THRESHOLD_HUM_L 0
+
+
+
+
 typedef struct {
   void (*configure_SYSTICK)(void);
   void (*configure_GPIO)(void);
@@ -20,6 +31,18 @@ setup = {
   .configure_ADC = configure_ADC
 };    // Esto no se si esta bien implementarlo en un header
       // pero deja limpio el main
+/**
+ * @brief Status of the analyze process.
+ * This will be triggered by the timer0 ISR
+ * Every 30 secs the system will allow the analyze process to be executed.
+ * This will function as an async process
+ */
+typedef enum {
+  ANALYZING,
+  ANALYZED,
+  CAN_ANALYZE,
+} analyze_flag_t;
+
 
 void configure_GPIO();
 void configure_SYSTICK();
@@ -35,10 +58,22 @@ uint16_t historic_vib[_MAX_VIB_N];  // vibraciones pasadas de los sismos
 uint16_t env_vib;
 uint16_t env_hum;
 
+analyze_flag_t analyze_proc_flag = CAN_ANALYZE; 
+
 int buzzer_mode; // estado del buzzer ON/OFF
 
 void update_env_state(uint16_t adc_vib, uint16_t adc_hum);
 void update_vib_frequency();
-
+/**
+ * @brief Analyze the environment info and 
+ * update the LEDs and alarm system.
+ * Alarm and LED system mechanism:
+ * -> Either vibrations frequency or humidity are above the threshold : RED LED
+ * -> Both vibrations frequency and humidity are above the threshold : RED LED + ALARM
+ * -> One of both is below the threshold : YELLOW LED
+ * -> Both are in the middle : YELLOW LED
+ * -> Both vibrations frequency and humidity are below the threshold : GREEN LED
+ */
+void analyze_and_update_system();
 
 uint16_t read_adc(uint8_t channel);
