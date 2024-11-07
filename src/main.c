399dbeec19e2d1be544f27cda7322fb2db9edb1b
Constanza Medran <134571030+constanzamedran@users.noreply.github.com>
2024-11-07 09:58:35 -0300

    config boton con int y systick

--- src/main.c
+++ src/main.c
@@ -1,180 +1,129 @@
+/**
+ * @file main.c
+ * @brief Toggles the LED on PC13 using a systick. Enable GPIO interrupt on PA0 to toggle the LED state.
+ */
+
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/cm3/systick.h>
 #include <libopencm3/stm32/exti.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
-#include <libopencm3/stm32/timer.h>
-#include <libopencm3/stm32/usart.h>
-#include <libopencm3/stm32/dma.h>
-#include <libopencm3/stm32/adc.h>
 
-#include "../include/util.h"
-#include "../include/pins.h"
+/* Constants */
+#define TRUE         1
+#define RELOAD_COUNT 8999 /* SysTick interrupt every N clock pulses: set reload to N-1 */
 
-#define TRUE 1
-#define FALSE 0
+/* Pin Definitions */
+#define LED_PORT    GPIOC
+#define LED_PIN     GPIO13 /* PC13 connected to onboard LED (on STM32F103C8T6 Blue Pill) */
+#define SWITCH_PORT GPIOA
+#define SWITCH_PIN  GPIO0 /* PA0 connected to switch */
 
-#define ON 1
-#define OFF 0
+/* Define SysTick interval */
+#define SYSTICK_INTERVAL_MS 100
 
-uint8_t usart1_rx_buffer[128]; // Define the buffer with an appropriate size
+/* Function Prototypes */
+void systemInit(void);
+void configure_gpio(void);
+void configure_systick(void);
+void toggle_led(void);
 
-int main() 
+/**
+ * @brief Initializes the system clock and peripherals.
+ */
+void systemInit(void)
 {
- 
-  execute_setup(&setup);
+    /* Configure the system clock to run at 72 MHz using an 8 MHz external crystal */
+    // Pro tip! To avoid warning messages, use the following syntax:
+    // rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
 
-  while(TRUE) 
-  {
-    if(analyze_proc_flag == CAN_ANALYZE)  // se ejecuta cada 30 segundos (timer0)
+    rcc_clock_setup_in_hse_8mhz_out_72mhz(); /* Use the default configuration */
+
+    /* Enable the clock for GPIOC */
+    rcc_periph_clock_enable(RCC_GPIOC);
+}
+
+/**
+ * @brief Configures GPIO pin PC13 as an output.
+ */
+void configure_gpio(void)
+{
+    /* Enable GPIO clocks */
+    rcc_periph_clock_enable(RCC_GPIOC);
+    rcc_periph_clock_enable(RCC_GPIOA);
+
+    /* Configure LED pin as output */
+    gpio_set_mode(LED_PORT, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, LED_PIN);
+
+    /* Configure switch pin as input with pull-up */
+    gpio_set_mode(SWITCH_PORT, GPIO_MODE_INPUT, GPIO_CNF_INPUT_PULL_UPDOWN, SWITCH_PIN);
+    gpio_set(SWITCH_PORT, SWITCH_PIN); /* Enable pull-up resistor */
+
+    /* Enable EXTI interrupt for the switch pin */
+    nvic_enable_irq(NVIC_EXTI0_IRQ); /* Enable EXTI0 interrupt (for PA0) */
+
+    exti_select_source(EXTI0, SWITCH_PORT);        /* Select PA0 as the source for EXTI0 */
+    exti_set_trigger(EXTI0, EXTI_TRIGGER_FALLING); /* Trigger on falling edge */
+    exti_enable_request(EXTI0);                    /* Enable EXTI0 interrupt request */
+}
+
+/**
+ * @brief Configures the systick timer.
+ */
+void configure_systick(void)
+{
+    systick_set_reload(rcc_ahb_frequency / 1000 * SYSTICK_INTERVAL_MS - 1); /* Set reload for SYSTICK_INTERVAL_MS */
+    systick_set_clocksource(STK_CSR_CLKSOURCE_AHB);
+    systick_counter_enable();
+    systick_interrupt_enable();
+}
+
+/**
+ * @brief SysTick interrupt handler.
+ */
+void sys_tick_handler(void)
+{
+    toggle_led();
+}
+
+/**
+ * @brief EXTI0 interrupt handler for switch press on PA0.
+ */
+void exti0_isr(void)
+{
+    if (exti_get_flag_status(EXTI0))
     {
-      analyze_proc_flag = ANALYZING; 
-      analyze_and_update_system();
-      analyze_proc_flag = ANALYZED;
+        exti_reset_request(EXTI0);
+        systick_counter_disable();
+        systick_interrupt_disable();
+         gpio_clear(LED_PORT, LED_PIN); //prende el led, el gpio_set apaga el led
+         //gpio_write_bit(LED_PORT, LED_PIN, 0); //prende el led 
+         //gpio_port_write(LED_PORT, 0x0000); //prende el led 
+}
+
+/**
+ * @brief Toggle the LED state.
+ */
+void toggle_led(void)
+{
+    gpio_toggle(LED_PORT, LED_PIN);
+}
+
+/**
+ * @brief Main function.
+ */
+int main(void)
+{
+    rcc_clock_setup_in_hsi_out_48mhz(); /* Set up clock, running at 48 MHz */
+
+    configure_gpio();    /* Configure GPIO pins */
+    configure_systick(); /* Configure SysTick timer */
+
+    nvic_set_priority(EXTI0, 0); /* Set priority for EXTI0 interrupt */
+
+    while (TRUE)
+    {
     }
-  }
-  return 0;
-}
-void analyze_and_update_system() // esto es asincrono a la interrupcion
-{
-   if (vib_freq > THRESHOLD_VIB_FREQ_H || env_hum > THRESHOLD_HUM_H) {
-    gpio_clear(LED_PORT, YELLOW_LED_PIN); 
-    gpio_clear(LED_PORT, GREEN_LED_PIN);
-    gpio_set(LED_PORT, RED_LED_PIN); 
-    buzzer_mode = OFF;
-      if(vib_freq > THRESHOLD_VIB_FREQ_H && env_hum > THRESHOLD_HUM_H)
-        buzzer_mode = ON; // alarma y led rojo
 
-  } else if(vib_freq <= THRESHOLD_VIB_FREQ_L || env_hum <= THRESHOLD_HUM_L) {
-    gpio_clear(LED_PORT, RED_LED_PIN); 
-    gpio_clear(LED_PORT, YELLOW_LED_PIN); 
-    gpio_set(LED_PORT, GREEN_LED_PIN); //led verde encendido
-    buzzer_mode = OFF; 
-  }//estado normal
-    else{  //cualquier estado amarillo 
-    gpio_clear(LED_PORT, RED_LED_PIN); 
-    gpio_clear(LED_PORT, GREEN_LED_PIN);
-    buzzer_mode = OFF; 
-    gpio_set(LED_PORT, YELLOW_LED_PIN); 
-  }
-}
-void update_vib_frequency()
-{
-  historic_vib[index_hist_vib] = env_vib;
-  index_hist_vib = (index_hist_vib + 1) % _MAX_VIB_N; // circular
-  if (index_hist_vib == 0) {
-    vib_freq = 0;
-    for (int i = 0; i < _MAX_VIB_N; i++) {
-      vib_freq += historic_vib[i];
-    }
-    vib_freq /= _MAX_VIB_N;   // promedio de las vibraciones
-  }
-}
-void update_env_state(uint16_t adc_vib, uint16_t adc_hum)
-{
-  env_hum = adc_hum;
-  env_vib = adc_vib;
-  //Aca deberiamos procesarlos un poco mas y ver que rango nos tira el adc
-} 
-void sys_tick_handler()
-{ 
-  //Convertir datos y guardarlos
-  update_env_state(read_adc(ADC_CHANNEL_hum), read_adc(ADC_CHANNEL_vib));
-  update_vib_frequency();
-}
-
-void timer0_isr()
-{
-  timer_clear_flag(TIM2, TIM_SR_UIF);
-  if(analyze_proc_flag == ANALYZED);
-    analyze_proc_flag = CAN_ANALYZE;
-
-}
-void exti0_isr()
-{
-  exti_reset_request(EXTI0);
-  buzzer_mode = !buzzer_mode;
-}
-void configure_GPIO()
-{
-  rcc_periph_clock_enable(RCC_GPIOC);
-  gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO8);
-}
-
-void configure_SYSTICK()
-{
-  systick_set_reload(STK_RVR_RELOAD);
-  systick_set_clocksource(STK_CSR_CLKSOURCE_AHB);
-  systick_counter_enable();
-}
-
-void configure_ADC() 
-{
-  rcc_periph_clock_enable(RCC_ADC1);
-  rcc_periph_clock_enable(RCC_GPIOA);
-  gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_ANALOG, GPIO0);
-  adc_power_off(ADC1);
-  adc_disable_scan_mode(ADC1);
-  adc_set_single_conversion_mode(ADC1);
-  adc_set_sample_time_on_all_channels(ADC1, ADC_SMPR_SMP_239DOT5CYC);
-  adc_power_on(ADC1);
-  for (int i = 0; i < 800000; i++) {
-    __asm__("nop");
-  } // Wait a bit before calibrating
-  adc_reset_calibration(ADC1);
-  adc_calibrate(ADC1);
-}
-
-void configure_TIMER()
-{
-  rcc_periph_clock_enable(RCC_TIM2);
-  timer_set_mode(TIM2, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
-  timer_set_prescaler(TIM2, 0x0000);
-  timer_set_period(TIM2, 0x0000);
-  timer_enable_irq(TIM2, TIM_DIER_UIE);
-  nvic_enable_irq(NVIC_TIM2_IRQ);
-  timer_enable_counter(TIM2);
-}
-
-void configure_DMA() 
-{
-  rcc_periph_clock_enable(RCC_DMA1);
-  dma_set_peripheral_address(DMA1, DMA_CHANNEL1, (uint32_t)&USART1_DR);
-  dma_set_memory_address(DMA1, DMA_CHANNEL1, (uint32_t)&usart1_rx_buffer);
-  dma_set_number_of_data(DMA1, DMA_CHANNEL1, sizeof(usart1_rx_buffer));
-  dma_set_memory_size(DMA1, DMA_CHANNEL1, DMA_CCR_MSIZE_8BIT);
-  dma_set_peripheral_size(DMA1, DMA_CHANNEL1, DMA_CCR_PSIZE_8BIT);
-  dma_enable_memory_increment_mode(DMA1, DMA_CHANNEL1);
-  dma_disable_peripheral_increment_mode(DMA1, DMA_CHANNEL1);
-  dma_enable_circular_mode(DMA1, DMA_CHANNEL1);
-  dma_set_read_from_peripheral(DMA1, DMA_CHANNEL1);
-  dma_enable_transfer_complete_interrupt(DMA1, DMA_CHANNEL1);
-  dma_enable_channel(DMA1, DMA_CHANNEL1);
-  nvic_enable_irq(NVIC_DMA1_CHANNEL1_IRQ);
-}
-
-void configure_UART()
-{
-  rcc_periph_clock_enable(RCC_USART1);
-  usart_set_baudrate(USART1, 9600);
-  usart_set_databits(USART1, 8);
-  usart_set_stopbits(USART1, USART_STOPBITS_1);
-  usart_set_parity(USART1, USART_PARITY_NONE);
-  usart_set_mode(USART1, USART_MODE_TX_RX);
-  usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
-  usart_enable(USART1);
-  usart_enable_rx_interrupt(USART1);
-  nvic_enable_irq(NVIC_USART1_IRQ);
-}
-
-void configure_EXTI()
-{
-  rcc_periph_clock_enable(RCC_GPIOA);
-  gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_PULL_UPDOWN, GPIO0);
-  gpio_set(GPIOA, GPIO0);
-  rcc_periph_clock_enable(RCC_AFIO);
-  exti_select_source(EXTI0, GPIOA);
-  exti_set_trigger(EXTI0, EXTI_TRIGGER_RISING);
-  exti_enable_request(EXTI0);
-  nvic_enable_irq(NVIC_EXTI0_IRQ);
+    return 0;
 }
